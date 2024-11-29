aaae56d20343886ed70c901ac4a14ad63e7ef8c9
Aleksey Bershov <bershov.aleksey@bk.ru>
2024-11-29 14:44:10 +0300

    refactor test_connect_db.php

--- test_connect_db.php
+++ test_connect_db.php
@@ -1,5 +1,4 @@
 <?php
-
 $server_name = "localhost";
 $user_name = "root";
 $password = "";
@@ -7,11 +6,16 @@ $db_name = "agv_db";
 
 $connect = new mysqli($server_name, $user_name, $password, $db_name);
 
+$response = [];
+
 if ($connect->connect_error) {
-    echo "false"; // Если есть ошибка подключения
+    $response['success'] = false;
+    $response['message'] = "Connection failed: " . $connect->connect_error;
 } else {
-    echo "true"; // Если подключение успешно
+    $response['success'] = true;
+    $response['message'] = "Connected successfully";
 }
 
 $connect->close();
+echo json_encode($response);
 ?>
